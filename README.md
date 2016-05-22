c1024b28f8e5727dfbb4bcf66fb4b68b689f5c37
Rubén Sánchez <ruben@ruben.(none)>
2016-05-22 11:47:17 +0200

    Este commit es la v1 de Hoteles

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # X-Nav-Practica-Hoteles
 Repositorio para la práctica final de DAT/AT. Curso 2015-2016
+
+Nombre y titulación:
+
+Rubén Sánchez Rivero. Ingeniería Telemática.
+
+Nombre de su cuenta en el laboratorio del alumno:
+
+rrivero
+
+Nombre de usuario en GitHub:
+
+slnruben
+
+Resumen de las peculiaridades que se quieran mencionar sobre lo implementado
+en la parte obligatoria:
+
+
+
+Lista de funcionalidades opcionales que se hayan implementado, y breve descripción de cada una:
+
+URL del vídeo demostración de la funcionalidad básica.
+
+URL del vídeo demostración de la funcionalidad optativa, si se ha realizado funcionalidad optativa:
+
+
+URL a la aplicación:
+
+<a href="http://slnruben.github.io/X-Nav-Practica-Hoteles">Hoteles</a>
---
+++ js/alojamientos.js
@@ -0,0 +1,245 @@
+var current;
+var collections = {};
+var housed = {};
+var hotel;
+
+var github;
+var token;
+
+var apiKey = "AIzaSyBSnpUqQ4eZDVP5cXey_HtY7U8G-fTcJac";
+
+$(function() {
+  $("#tabs").tabs({disabled: [1, 2]});
+});
+
+function handleClientLoad() {
+  gapi.client.setApiKey(apiKey);
+}
+
+function getToken() {
+  token = $("#inputToken").val();
+  github = new Github({
+    token: token,
+    auth: "oauth"
+  });
+};
+
+function showRepo(error, repo) {
+  if (error) {
+    alert("Error code: " + error.error);
+    return false;
+  } else {
+    $("#inputToken").hide();
+    console.log("<p>Repo data:</p>" +
+      "<ul><li>Full name: " + repo.full_name + "</li>" +
+      "<li>Description: " + repo.description + "</li>" +
+      "<li>Created at: " + repo.created_at + "</li>" +
+      "</ul>");
+  }
+  return true;
+};
+
+function getRepo() {
+  var user = $("#inputUser").val();
+  var reponame = $("#inputRepo").val();
+  var repo = github.getRepo(user, reponame);
+  repo.show(showRepo);
+  console.log("repo " + repo);
+  return repo;
+};
+
+function writeFile(repo, name, json) {
+  var name = $("#nameFile").val();
+  repo.write('gh-pages', name, json, 'save data', function(err) {});
+}
+
+function saveData() {
+  if (!token) {
+    getToken();
+  }
+  var repo = getRepo();
+  if (collections) {
+    var jcollections = JSON.stringify(collections);
+    writeFile(repo, "collections.json", jcollections);
+  }
+  if (housed) {
+    var jhoused = JSON.stringify(housed);
+    writeFile(repo, "housed.json", jhoused);
+  }
+     
+  console.log(f);
+}
+
+function addUser(userInfo) {
+  var element = document.createElement("li");
+  element.innerHTML = '<img src="' + userInfo.image + '">' + userInfo.name;
+  $(".housedList").append(element);
+}
+
+function getHousedList() {
+  $("#hostList").html('<ul class="hotel housedList"></ul>');
+  for (var i = 0; i < housed[hotel].length; i++) {
+    addUser(housed[hotel][i]);
+  }
+}
+
+function makeApiCall(id) {
+  gapi.client.load('plus', 'v1', function() {
+    var request = gapi.client.plus.people.get({
+      'userId': id,
+    });
+    request.execute(function(resp) {
+      if (resp) {
+        var userInfo = {
+          id: resp.id,
+          name: resp.displayName,
+          image: resp.image.url,
+        };
+        for (var i = 0; i < housed[hotel].length; i++) {
+          if (housed[hotel][i].name == userInfo.name) {
+            return;
+          }
+        }
+        housed[hotel].push(userInfo); 
+        addUser(userInfo);
+      } else {
+        alert("The id is incorrect");
+      }
+    });
+  });
+}
+
+function accomodateUser() {
+  var id = $("#inputHost").val();
+  $("#inputHost").val("");
+  makeApiCall(id);
+}
+
+function showCollection() {
+  if (current) {
+    current.toggleClass("blue");
+  }
+  current = $(this);
+  current.toggleClass("blue");
+  var name = current.html();
+  var list = '<ul class="hotel currentList">';
+  for (var i = 0; i < collections[name].length; i++) {
+    list = list + '<li class="hotelCollection">' + collections[name][i] + '</li>';
+  }
+  list = list + '</ul>';
+  $("#currentCollection").html("Current Collection: " + name + list);
+  $("#collection").html("Hotels in: " + name + list);
+  $(function() {
+    $("#currentCollection ul").droppable({
+      drop: function (event, ui) {
+        if (collections[name].indexOf(ui.draggable.text()) == -1) {
+          collections[name].push(ui.draggable.text());
+          var element = document.createElement("li");
+          element.className = "hotelCollection";
+          element.innerHTML = ui.draggable.text();
+          $(".currentList").append(element);
+          $(this).find(".placeholder").remove();
+        }
+      }
+    });
+  });
+}
+
+function createCollection() {
+  var name = $("#inputName").val();
+  if (name) {
+    if ($("#createdCollections li." + name).length == 0) {
+      $("#createdCollections").append('<li class="collection ' + name + '">' + name + '</li>');
+      $("li." + name).click(showCollection);
+      collections[name] = [];
+    } 
+  }
+}
+
+function onPopupOpenFactory(hotel) {
+  return function(){
+    var tempMarker = this;
+    $(".marker-delete-button:visible").click(function () {
+      hotel.toggleClass("green");
+      map.removeLayer(tempMarker);
+    });
+  }  
+}
+
+function makeCarousel(arr) {
+  $(".carousel-indicators").html("");
+  $(".carousel-inner").html("");
+  $(".glyphicon").show();
+  for (var i = 0; i < arr.length; i++) {
+    $(".carousel-inner").append('<div class="item"><img src="' + arr[i].url + '"><div class="carousel-caption"></div></div>');
+    $(".carousel-indicators").append('<li data-target="#carousel-example-generic" data-slide-to="' + i + '"></li>');
+  }
+  $(".item").first().addClass("active");
+  $(".carousel-indicators > li").first().addClass("active");
+  $("#carousel-example-generic").carousel();
+}
+
+function showAccomodation() {
+  $("#tabs").tabs("enable", 2);
+  var accomodation = accomodations[$(this).attr("no")];
+  $(this).toggleClass("green");
+  if ($(this).hasClass("green")) {
+    var lat = accomodation.geoData.latitude;
+    var lon = accomodation.geoData.longitude;
+    var url = accomodation.basicData.web;
+    var name = accomodation.basicData.name;
+    var desc = accomodation.basicData.body;
+    makeCarousel(accomodation.multimedia.media);
+    var cat = accomodation.extradata.categorias.categoria.item[1]['#text'];
+    var subcat = accomodation.extradata.categorias.categoria
+     .subcategorias.subcategoria.item[1]["#text"];
+    marker = L.marker([lat, lon]).addTo(map)
+     .bindPopup('<a href="' + url + '">' + name + '</a><br/><input type="button" value="Delete this marker" class="marker-delete-button"/>');
+    marker.on("popupopen", onPopupOpenFactory($(this)));
+    marker.openPopup();
+    map.setView([lat, lon], 15);
+    $(".info").html('<h2>' + name + '</h2>'
+     + '<p>Type: ' + cat + ', subtype: ' + subcat + '</p>'
+     + desc);
+    hotel = name;
+    getHousedList();
+  } else {
+    map.removeLayer(marker);
+  }
+};
+
+function get_accomodations() {
+  $.getJSON("alojamientos.json", function(data) {
+    $("#get").hide();
+    $("#tabs").tabs("enable", 1);
+    $("#collection").prop("hidden", false);
+    accomodations = data.serviceList.service;
+    $("#result").html("Accomodations found: " + accomodations.length
+     + " (click on any of them for details and location in the map)");
+    var list = '<ul class="hotel">';
+    for (var i = 0; i < accomodations.length; i++) {
+      housed[accomodations[i].basicData.title] = [];
+      list = list + '<li class="hotel" no=' + i + '>' + accomodations[i].basicData.title + '</li>';
+    }
+    list = list + '</ul>';
+    $(".list").html(list);
+    $("#generalList li").click(showAccomodation);
+    $(function() {
+      $("#collectionList li").draggable({
+        helper: "clone",
+      });
+    });
+
+  });
+};
+
+$(document).ready(function() {
+  map = L.map("map").setView([40.4175, -3.708], 11);
+  L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+  $("#get").click(get_accomodations);
+  $("#create").click(createCollection);
+  $("#host").click(accomodateUser);
+  $("#save").click(saveData);
+});
